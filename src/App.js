590032b57202565502cc63acd3bd854e43968037
chaserChoi <chlwogur2010@gmail.com>
2023-10-04 13:15:43 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Movie from './components/Movie';
+import { movies } from './movieList';
+
+function App() {
+  return (
+    <div>
+      <div className="app-container">
+        {
+          movies.results.map((item) => {
+            return (
+              <Movie 
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+              />
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
